8d935c052a181902dfe75dbdc8c5139c0a512905
rahatlou <rahatlou@gmail.com>
2018-10-15 17:32:42 +0200

    added html index

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Shahram Rahatlou" />
+<meta name="Description" content="Computing Methods in Physics" />
+
+
+<title>Computing Methods in Physics, Shahram Rahatlou</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<!-- google analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-35197438-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-35197438-3');
+</script>
+
+<!-- google analytics -->
+
+
+</head>
+<body>
+<div id="header">
+	<div id="logo">
+		<h1>Computing Methods in Physics</h1>
+	</div>
+	<!-- end #logo -->
+	<div id="cssmenu">
+          <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li class='has-sub'><a href="../didattica.php">Didattica</a>
+               <ul> 
+                 <li><a href="../labcalc/">Lab Calcolo</a></li>
+                 <li><a href="../particelle/">Particelle Elementari</a></li>
+                 <li><a href="../programmazione++/">Programmazione++</a></li>
+                 <li><a href="../labhep/">Lab Particelle</a></li>
+               </ul> 
+            </li>
+            <li><a href="../contact.php">Contact &amp; Calendar</a></li>
+          </ul>
+	</div>
+	<!-- end #menu -->
+</div>
+<!-- end #header -->
+
+<div id="page">
+   <div id="content">
+     <?php include("frontpage.html") ?>
+   </div>
+   <!-- end #content -->
+
+   <div id="sidebar">
+     <div id="sidebar-bgtop"></div>
+     <div id="sidebar-content">
+           <h3 class=h3sidebar>Last update: 12 Oct 2018</h3>
+
+            Created by 
+            <a href="http://www.roma1.infn.it/people/rahatlou/">Shahram Rahatlou</a>
+            [<a href="mailto:shahram.rahatlou@uniroma1.it">shahram.rahatlou (at) uniroma1.it</a>]
+
+
+          <p>
+          <h2>Avvisi</h2>
+          <div style="width: 270px; height: 250px; overflow: auto">
+          <ul>  <!-- lista avvisi-->
+             <li>3/10: Please note the change of room for all lectures starting Monday 8 October</li>
+             <li>3/10: There will be 4 additional lectures during the month of October on Monday 8, 15, 22, 29 at 14:00 in Aula 4, Nuovo Edificio di Fisica</li>
+             <li>25/9: Le lezioni avranno inizio il giorno martedi` 25 settembre</li>
+          </ul> <!-- lista avvisi-->
+          </div>
+
+          <h2>Testo Consigliato</h2>
+
+           <ul>
+              <li>Lecture notes</li>
+              <li>additional material will be provided during the course</li> 
+           </ul>
+
+          <h2 class="title">Orario Ricevimento</h2>
+           <ul>
+             <li>Lunedi` 13-15, <a href="http://www.phys.uniroma1.it/mappe/mappe.php?edificio=marconi&piano=2">Stanza 202-A, Piano 2, Edificio Marconi (Vecchio edificio di Fisica)</a>
+             </li>
+           </ul>
+
+
+
+<p>
+<!-- Place this tag where you want the +1 button to render. -->
+<div class="g-plusone" data-annotation="inline" data-width="300"></div>
+
+<!-- Place this tag after the last +1 button tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+
+     </div>
+
+     <div id="sidebar-bgbtm">
+     </div>
+
+   </div>
+   <!-- end #sidebar -->
+
+</div>
+<!-- end #page -->
+
+<div id="footer">
+<p>&copy; 2008. All Rights Reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+</div>
+
+
+<!-- end #footer -->
+
+<!-- Start of StatCounter Code for Default Guide -->
+<!-- End of StatCounter Code for Default Guide -->
+
+
+</body>
+</html>
