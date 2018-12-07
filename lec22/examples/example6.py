40d580c04cfb34d3ee98f5b1d1f65035560aa1e8
rahatlou <rahatlou@gmail.com>
2018-12-07 15:04:03 +0100

    updated

--- lec22/examples/example6.py
+++ lec22/examples/example6.py
@@ -1,7 +1,19 @@
 import mymodule
 
-print("++++ executing " +  __file__ + " with name space " + __name__)
+print("++++ executing name space "+ __name__ +  __file__)
 
+# local a variable
+a = 'test string'
+
+# any object in mymodule can be used
+# and there is no confusion with local a
+print( "mymodule.a: %f"%mymodule.a )
+print( "local a: " + a )
+
+## use line function from mymodule
 print( mymodule.line( 2.34, q=0.5 )  )
 
-print( "mymodule.a: %f"%mymodule.a )
+## function p1 is defined in mymodule but cannot be used because
+## behind __name__ == "__main__" in mymodule
+##
+print( mymodule.p1( 2.34, q=0.5 )  )
