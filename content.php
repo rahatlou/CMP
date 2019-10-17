e71e08913b7599afc90dc8707a8c98b94e44b753
rahatlou <rahatlou@gmail.com>
2019-10-17 17:28:13 +0200

    first version of 2019 site with new format

---
+++ content.php
@@ -0,0 +1,287 @@
+<div id="content">
+   <h1>Computing Methods for Physics</h1>
+	
+   <div class="post">
+
+   <h2 class="title">Orario Lezioni</h2>
+   <div class="entry">
+   <p><b>Lectures start on Tuesday 25 September 2019</b>
+   <ul>
+     <li> Tuesday 12-14, Aula 7 , Nuovo Edificio di Fisica</li>
+     <li> Friday  12-14, Aula 6 , Nuovo Edificio di
+   Fisica</li>
+     <li> Lab Sessions: there will be about 5-6 lab sessions on Monday 10-12 in Laboratorio di Calcolo, Nuovo Edificio di Fisica. The dates will be discussed and set at the beginning of the course based on the number of attendants.</li>
+   </ul>
+   </div> <!-- entry -->
+
+   <h2 class="title">Student survey</h2>
+   <div class="entry">
+   <p>Please fill this informal <a href="https://docs.google.com/forms/d/e/1FAIpQLScMwRWGDYLbphrKOBqoEToqhGm-L4pg8DO7x8cAKFUt3dficw/viewform?usp=sf_link">survey module</a> by Tuesday 1 Oct to help me with the organisation of the course and lab sessions.</p>
+   <p>You must use your uniroma1 credentials. In case of problems, please restart your browser  or log out of your private google account before accessing the form.</p>
+
+   </div>
+
+
+
+   
+<div class="post">
+
+<h2 class="title">Material</h2>
+<div class="entry">
+<!-- begin lectures -->
+<ul>
+
+<li>
+Lec 01, 25/9: Introduction to the course (<a href="https://github.com/rahatlou/CMP/tree/master/lec00/introduction.pdf">introduction</a>). Introduction to C++.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec01/lec01.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec01/examples">examples</a>)
+</li>
+
+<li>Lec 02, 27/9: References and pointers in C++. Constant. Namespace. Introduction to Class. 
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec02/lec02.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec02/examples">examples</a>)
+</li>
+
+<li>
+Lec 03,  1/10: Classes and objects in C++. Interface and attributes. Constructors. Separating interface and implementation. Header and source files.
+(<a href="lec03.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec03/examples">examples 1</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec04/examples">examples 2</a>)
+</li>
+
+<li>
+Lec 04, 4/10: Header and source files. Dynamic memory allocation. Destructor. Using class std::vector.
+(<a href="lec04.pdf">pdf</a>, 
+<a href="https://github.com/rahatlou/CMP/tree/master/lec04/examples">examples 1</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec05/examples">examples 2</a>)
+</li>
+
+<li>
+Lec 05, 8/10: Destructor. const member functions. Default arguments for functions. Applications in C++. Arguments from command line. external libraries. Introduction to ROOT.
+(<a href="lec05.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec05/examples">examples 1</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec06/examples">examples 2</a>)
+</li>
+
+<li>
+Lec 06, 15/10:
+Numerical convolution: Gaussian detector resolution and monochromatic source. Gaussian convolution of
+exponetial decay length distribution.
+Dynamically allocated object as data members. Overloading operators. Adding operators to class Datum. 
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec07/lec07.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec07/examples">examples</a>)
+</li>
+
+<li>
+Lec 07, 18/10: Overloading operators and friend methods. class Vector3D. Static data members and
+methods. Application to compute weighted average. 
+</li>
+
+
+<!--
+   
+<li>
+Lec 01, 25/9: Introduction to the course (<a href="lec00/introduction.pdf">introduction</a>). Introduction to C++.
+(<a href="lec01/lec01.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec01/examples">examples</a>)
+</li>
+
+<li>Lec 02, 28/9: References and pointers in C++. Constant. Namespace. Introduction to Class.
+(<a href="lec02/lec02.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec02/examples">examples</a>)
+</li>
+
+<li>
+Lec 03,  2/10: Classes and objects in C++. Interface and attributes. Constructors.
+(<a href="lec03/lec03.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec03/examples">examples</a>)
+</li>
+
+<li>
+Lec 04, 5/10: Separating interface and implementation. Header and source files.
+(<a href="lec04/lec04.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec04/examples">examples</a>)
+</li>
+
+<li>
+Lec 05, 8/10: Using class std::vector. Dynamic memory allocation. Destructor. const member functions. Default arguments for functions.
+(<a href="lec05/lec05.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec05/examples">examples</a>)
+</li>
+
+<li>
+Lec 06, 9/10: Applications in C++. Arguments from command line. external libraries. Introduction to ROOT.
+(<a href="lec06/lec06-application.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec06/examples">examples</a>)
+</li>
+
+<li>
+Lec 07, 12/10:
+Dynamically allocated object as data members. Overloading operators. Adding operators to class Datum. class Vector3D.
+(<a href="lec07/lec07.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec07/examples">examples</a>)
+</li>
+
+<li>
+Lec 08, 15/10:
+Overloading operators and friend methods. Static data members and
+methods. Application to compute weighted average.
+(<a href="lec08/lec08.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec08/examples">examples</a>)
+</li>
+
+<li>
+Lec 09, 16/10:
+Example of static data member in class Datum. Enumerators. use case for
+std::map, std::pair, and std::vector
+(<a href="lec09/lec09.pdf">pdf</a>, <a href="https://github.com/rahatlou/CMP/tree/master/lec09/examples">examples</a>)
+</li>
+
+<li>
+Lec 10, 19/10:
+Input/Output with TTree and TFile (<a href="https://github.com/rahatlou/CMP/tree/master/lec10/examples">examples</a>)
+</li>
+
+<li>
+Lec 11, 23/10:
+I/O with TTree: variable size arrays. Storing user-defined Class in
+TTree
+(<a href="https://github.com/rahatlou/CMP/blob/master/lec11/lec11.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec11/examples">examples</a>,
+<a
+href="https://github.com/rahatlou/CMP/blob/master/misc/ROOT.md">ROOT setup and configuration</a>)
+<br>
+This lecture has been written with the <a
+href="https://en.wikipedia.org/wiki/Markdown"  target="_blank">Markdown</a>
+language. The graphical viewer is provided by <a
+href="https://github.com"  target="_blank">GitHub</a>
+
+<li>
+Lec 12, 26/10:
+Application for analysis of TTree. Creating custom analysis class.
+(<a
+href="https://github.com/rahatlou/CMP/blob/master/lec12/lec12.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec12/examples">examples</a>)
+</li>
+
+
+<li>
+Lec 13, 6/11:
+Object oriented programming: Inheritance. Base and derived class.
+(<a href="lec13/lec13.pdf">pdf</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec13/examples">examples</a>)
+</li>
+
+
+<li>
+Lec 14, 9/11:
+Polymorphism: abstract class and virtual methods. Applications.
+(<a href="lec14/lec14.pdf">pdf</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec14/examples0">examples I</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec14/examples1">examples II</a>)
+</li>
+
+<li>
+Lec 15, 13/11:
+Strategy pattern,
+examples and applications: Numerical integration methods. custom Function class.<br>
+Composition pattern, examples and applications: leaf and composites in graphical applications.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec15/lec15.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec15/examples">examples</a>)
+</li>
+
+<li>
+Lec 16, 20/11:
+Composition pattern: leaf and composites in graphical applications.
+Example of a solar system simulation with composite objects.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec16/lec16.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec16/examples">examples</a>)
+</li>
+
+<li>
+Lec 17, 23/11:
+Composition pattern: examples in high energy phyiscs: tracks, photons, electrons, jets.
+Midterm test (<a href="midterm/midterm.pdf">pdf</a>)
+</li>
+
+<li>
+Lec 18, 27/11:
+Templates and generic programming in C++. Template functions and classes.
+ (<a href="https://github.com/rahatlou/CMP/tree/master/lec18/lec18.pdf">pdf</a>,
+ <a href="https://github.com/rahatlou/CMP/tree/master/lec18/examples">examples</a>)
+</li>
+
+<li>
+Lec 19, 30/11:
+Discussion of midterm test and solution: Pros and cons of using void functions with
+non-const pointers. Polymorphic detectors.
+</li>
+
+<li>
+Lec 20, 3/12:
+Template class. STL containers. Error handling with exceptions.
+ (<a href="https://github.com/rahatlou/CMP/tree/master/lec20/lec20.pdf">pdf</a>,
+ <a href="https://github.com/rahatlou/CMP/tree/master/lec20/examples">examples</a>)
+</li>
+
+
+<li>
+Lec 21, 4/12: Introduction to Python. Main differences with C/C++.
+Introduction to <a href="https://jupyter.org">jupyter</a>.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec21/lec21.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec21/examples">examples</a>)
+<a href="https://github.com/rahatlou/CMP/tree/master/misc/DownloadNotebook.md">Instructions for downloading the lecture notebook</a>
+</li>
+
+<li>
+Lec 22, 7/12: basics of python: semantics, flow control, data types, functions, modules.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec22/lec22.ipynb">nb</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec22/examples">examples</a>)
+</li>
+
+<li>
+Lec 23, 10/12: data types in python: sequences. Lists, Tuples. Example of
+plotting with matplotlib
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec23/README.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec23/examples">examples</a>)
+</li>
+
+<li>
+Lec 24, 11/12: data types in python: Dictionaries and sets. Comprehensions.
+data anlaysis with sets, lists, dicts. Plotting a histogram. revisiting example from
+previous lecture.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec24/README.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec24/examples">examples</a>)
+</li>
+
+<li>
+Lec 25, 14/12: More on Functions in python. Animated plots with matplotlib.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec25/README.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec25/examples">examples</a>)
+</li>
+
+<li>
+Lec 26, 21/12: Numpy and array-oriented programming with ndarray. Example of random walk with arrays.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec26/README.md">md</a>)
+</li>
+
+<li>
+Lec 27, 8/1: Classes, inheritance, and polymorphism in Python
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec27/README.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec27/examples">examples</a>)
+</li>
+
+<li>
+Lec 28, 11/1: File I/O and handling in python. Functions with variable number of arguments.
+Command line arguments for python programs
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec28/README.md">md</a>,
+<a href="https://github.com/rahatlou/CMP/tree/master/lec28/examples">examples</a>)
+</li>
+
+<li>
+Lec 29, 14/1: Photons and hadronic jets in proton collisions.
+Brief introduction to  Machine Learning and its applications.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec29/lec29.md">md</a>)
+</li>
+
+<li>
+Lec 30, 15/1: Types of Machine Learning and main challenges.
+Example of b-tagging with machine learning.
+(<a href="https://github.com/rahatlou/CMP/tree/master/lec30/lec30.md">md</a>)
+</li>
+										   
+-->
+   
+</ul>
+<!-- end of lectures -->
+  </div> <!--- end of entry -->
+
+
+
+  </div> <!--- end of post -->
+
+</div> <!-- end of content -->
