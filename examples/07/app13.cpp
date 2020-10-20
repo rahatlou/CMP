227d87f0626c44ea03072da6c8b874c21c2f7326
rahatlou <rahatlou@gmail.com>
2020-10-20 11:27:20 +0200

    updated examples

--- examples/07/app13.cpp
+++ examples/07/app13.cpp
@@ -1,20 +1,16 @@
-// app4.cpp
+// app3.cpp
 #include <iostream>
 using namespace std;
 
-#include "Datum.h"
+#include "Datum1.h"
 
 
 int main() {
   const Datum d1( 1.2, 0.3 );
-  Datum d2( -0.4, 0.4 );
+  const Datum d2( -0.4, 0.4 );
 
-  Datum d3 = d1;
+  Datum d3 = d1 + d2;
   d3.print();
-
-  Datum d4;
-  d4.operator=(d2);
-  d4.print();
 
   return 0;
 }
