1f5ec56d138e3c4f3e6180f61e48f7230fe3cba8
rahatlou <rahatlou@gmail.com>
2021-02-09 18:19:28 +0100

    aggiornamento per febbraio

--- exams/content.php
+++ exams/content.php
@@ -26,12 +26,28 @@
 			between 10:30 and 12:30.</p>
 
 			<p>
-			The oral discussion will be on 15 and 17 Feb on
+			The oral discussion will be on 11, 16 and 17 Feb on
 			<a href="https://meet.google.com/vbb-btpf-uzb">Google Meet code: vbb-btpf-uzb</a>.</p>
 
 			<p>Students will indicate their preferred
 			date and time, when submitting their exam.
 			</p>
+
+
+			<p>A valid Sapienza email address is mandatory to particpate in the exam and submit your project.</p>
+
+			<p>Students who have made a reservation on infostud for this sessions can submit their project with the following online procedure
+			<ul>
+				<li>Log into <a href="https://mail.uniroma1.it">https://mail.uniroma1.it</a>
+						with your Sapienza credentials. (if you are already logged in with your personal gmail account, you must first log out)</li>
+				<li>Go to <a href="https://docs.google.com/forms/d/e/1FAIpQLSeFoO7pKkt7aHLrCM6n5gBPBA_63mvA0Rn-2EAs61Mole0cug/viewform?usp=sf_link"  target="_blank">google form</a> </li>
+				<li>You can submit only one zip file containing your files</li>
+				<li>All C++, python, pdf, or jupyter files <b>must</b> be in the zip file. You cannot submit individual files.</li>
+				<li>The form will close automatically at <b>13:30</b>.</li>
+			</ul>
+			</p>
+
+
 
 
 		<a id="20210127"></a>
